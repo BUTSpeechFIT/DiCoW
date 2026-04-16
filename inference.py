ce3f844a236854d1fdce935ef40a137e1e955990
yro7 <yronusa2000@gmail.com>
2026-04-16 14:17:04 +0200

    fix: update inference & dicow model's code to support transformers 4.40.1+ (and to use local model)

--- inference.py
+++ inference.py
@@ -5,6 +5,7 @@ import glob
 from pathlib import Path
 
 from transformers import AutoTokenizer, AutoFeatureExtractor, AutoModelForSpeechSeq2Seq
+
 from pipeline import DiCoWPipeline
 from diarizen.pipelines.inference import DiariZenPipeline
 
@@ -179,8 +180,9 @@ def main():
     # Load models
     if args.verbose:
         print("Loading DiCoW model...")
-
-    dicow = AutoModelForSpeechSeq2Seq.from_pretrained(args.dicow_model, trust_remote_code=True)
+    # I use the "local" fixed version instead of the HF one, that still relies on the old transformers version
+    from models.dicow.modeling_dicow import DiCoWForConditionalGeneration
+    dicow = DiCoWForConditionalGeneration.from_pretrained(args.dicow_model)
     feature_extractor = AutoFeatureExtractor.from_pretrained(args.dicow_model)
     tokenizer = AutoTokenizer.from_pretrained(args.dicow_model)
     create_lower_uppercase_mapping(tokenizer)
